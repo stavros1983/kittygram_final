ddb6ffbaea9103c454581b3c18ce930c89a24ca9
Бабаян Армен <stavros1983@yandex.ru>
2023-08-12 05:36:16 +0400

    ah3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = False
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', "127.0.0.1").split()
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
